a8b8b9f057b2e0813e1e4a72889a03dfad513cbf
Alex Hosszu <hosszualex99@gmail.com>
2019-07-16 18:00:11 +0300

    Finished all of the requirements. Only thing left to implement is the "Keep me signed in" radio button.

---
+++ app/src/main/java/com/example/loginapp/HomeScreen.java
@@ -0,0 +1,25 @@
+package com.example.loginapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        final TextView welcome = findViewById(R.id.textview_welcome);
+
+        Intent iUsername = getIntent();
+        final String email_passed = iUsername.getStringExtra("KEY");
+
+        final String username = email_passed.substring(0,email_passed.indexOf("@"));
+
+        welcome.setText("Welcome to the ZOO, " + username + "!");
+
+    }
+}
--- app/src/main/java/com/example/loginapp/UserLogin.java
+++ app/src/main/java/com/example/loginapp/UserLogin.java
@@ -1,8 +1,13 @@
 package com.example.loginapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserLogin extends AppCompatActivity {
 
@@ -12,5 +17,61 @@ public class UserLogin extends AppCompatActivity {
         setContentView(R.layout.activity_user_login);
 
         Button loginButton = findViewById(R.id.button_login);
+
+        final String x1 = "hosszualex99@gmail.com";
+        final String x2 = "1234";
+
+        final EditText email = findViewById(R.id.edit_text_email);
+        final EditText pass = findViewById(R.id.edit_text_password);
+        final TextView error = findViewById(R.id.textview_error);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                error.setText("");
+                email.setHintTextColor(Color.GRAY);
+                pass.setHintTextColor(Color.GRAY);
+            }
+        });
+
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                error.setText("");
+                email.setHintTextColor(Color.GRAY);
+                pass.setHintTextColor(Color.GRAY);
+            }
+        });
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals(x1)&&pass.getText().toString().equals(x2))
+                {
+                    Intent loginIntent = new Intent(getApplicationContext(), HomeScreen.class);
+                    loginIntent.putExtra("KEY",x1);
+                    startActivity(loginIntent);
+                }
+                else
+                {
+                    if(email.getText().toString().equals(""))
+                    {
+                        email.setHintTextColor(Color.RED);
+                        email.setHint("Input an e-mail");
+                    }
+                    if(pass.getText().toString().equals(""))
+                    {
+                        pass.setHintTextColor(Color.RED);
+                        pass.setHint("Input a password");
+                    }
+
+                    if((!email.getText().toString().equals("") && !email.getText().toString().equals(x1))||
+                            (!pass.getText().toString().equals("")&&!pass.getText().toString().equals(x2)))
+                        error.setText("e-mail and/or password are invalid");
+                }
+            }
+        });
+
     }
 }
